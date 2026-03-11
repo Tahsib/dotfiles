ae3df90c6a45b51ed940c491a40b527b6267c38b
Tahsib <tahsib7@gmail.com>
2026-03-11 09:40:26 +0600

    chore: update config for cross-platform support and normalize nvim formatting

--- Makefile
+++ Makefile
@@ -1,6 +1,24 @@
-tmux:
-	cat ${PWD}/.tmux.conf > ~/.tmux.conf
+# Symlink configurations to $HOME
+# Use -f to force/overwrite existing files
+# Use -s for symbolic links (prevents copying)
+
+all: nvim tmux ghostty kitty aliases
 
 nvim:
-	mkdir -p ~/.config 
-	cp -r ${PWD}/nvim ~/.config/
+	mkdir -p ~/.config
+	ln -sf $(PWD)/nvim ~/.config/nvim
+
+tmux:
+	ln -sf $(PWD)/.tmux.conf ~/.tmux.conf
+
+ghostty:
+	mkdir -p ~/.config/ghostty
+	ln -sf $(PWD)/ghostty/config ~/.config/ghostty/config
+
+kitty:
+	mkdir -p ~/.config/kitty
+	ln -sf $(PWD)/kitty/kitty.conf ~/.config/kitty/kitty.conf
+
+aliases:
+	# Note: This just links the file. You still need to source it in ~/.zshrc or ~/.bashrc
+	ln -sf $(PWD)/aliases ~/.aliases
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,63 +1,65 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls", "golangci_lint_ls", "html", "ts_ls", "pylsp", "terraformls", "yamlls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-      local util = require("lspconfig/util")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-        cmd = {"gopls"},
-        filetypes = { "go", "gomod", "gowork", "gotmpl"},
-        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-        settings = {
-          gopls = {
-            completeUnimported = true,
-            usePlaceholders = true,
-          }
-        }
-			})
-      lspconfig.golangci_lint_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pylsp.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.terraformls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.yamlls.setup({
-				capabilities = capabilities,
-			})
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "gopls", "golangci_lint_ls", "html", "ts_ls", "pylsp", "terraformls", "yamlls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show Hover Docs" })
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to References" })
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol" })
-			vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show Line Diagnostics" })
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
-		end,
-	},
+      -- Neovim 0.11+ "Core" Setup Pattern
+      local servers = {
+        lua_ls = {},
+        gopls = {
+          cmd = { "gopls" },
+          filetypes = { "go", "gomod", "gowork", "gotmpl" },
+          root_dir = function(fname)
+            return vim.fs.root(fname, { "go.work", "go.mod", ".git" })
+          end,
+          settings = {
+            gopls = {
+              completeUnimported = true,
+              usePlaceholders = true,
+            },
+          },
+        },
+        golangci_lint_ls = {},
+        html = {},
+        pylsp = {},
+        terraformls = {},
+        yamlls = {},
+      }
+
+      for server, config in pairs(servers) do
+        config.capabilities = capabilities
+        -- Use the new 0.11 vim.lsp.config if available, fallback to legacy for compatibility
+        if vim.lsp.config then
+          vim.lsp.config(server, config)
+        else
+          require("lspconfig")[server].setup(config)
+        end
+      end
+
+      -- Keybindings
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show Hover Docs" })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to References" })
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol" })
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show Line Diagnostics" })
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
+    end,
+  },
 }
