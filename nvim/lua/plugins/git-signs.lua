959a9ffa6bdc11aa61eacebde0b602d01b541665
Tahsib <tahsib7@gmail.com>
2026-05-23 04:13:08 +0600

    feat: implement pre-commit hooks for industry-standard linting and formatting

--- nvim/lua/plugins/git-signs.lua
+++ nvim/lua/plugins/git-signs.lua
@@ -12,27 +12,37 @@ return {
         end
 
         -- Navigation
-        map('n', ']c', function()
-          if vim.wo.diff then return ']c' end
-          vim.schedule(function() gs.next_hunk() end)
-          return '<Ignore>'
-        end, {expr=true, desc = "Next Git Hunk"})
+        map("n", "]c", function()
+          if vim.wo.diff then
+            return "]c"
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true, desc = "Next Git Hunk" })
 
-        map('n', '[c', function()
-          if vim.wo.diff then return '[c' end
-          vim.schedule(function() gs.prev_hunk() end)
-          return '<Ignore>'
-        end, {expr=true, desc = "Previous Git Hunk"})
+        map("n", "[c", function()
+          if vim.wo.diff then
+            return "[c"
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true, desc = "Previous Git Hunk" })
 
         -- Actions
-        map('n', '<leader>hp', gs.preview_hunk, { desc = "Preview Hunk" })
-        map('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = "Blame Line" })
-        map('n', '<leader>hs', gs.stage_hunk, { desc = "Stage Hunk" })
-        map('n', '<leader>hr', gs.reset_hunk, { desc = "Reset Hunk" })
-        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = "Stage Buffer" })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = "Reset Buffer" })
-      end
+        map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview Hunk" })
+        map("n", "<leader>hb", function()
+          gs.blame_line({ full = true })
+        end, { desc = "Blame Line" })
+        map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage Hunk" })
+        map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset Hunk" })
+        map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
+        map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage Buffer" })
+        map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset Buffer" })
+      end,
     })
-  end
+  end,
 }
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,19 +2,19 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   config = function()
-    require("toggleterm").setup{
+    require("toggleterm").setup({
       size = 15,
       open_mapping = [[<c-\>]],
       on_open = function()
-        vim.cmd("startinsert")  -- Automatically start in terminal mode
+        vim.cmd("startinsert") -- Automatically start in terminal mode
       end,
       on_close = function()
         vim.cmd("wincmd p")
       end,
       auto_scroll = true,
       autochdir = true,
-    }
-  -- Keybinding to open toggleterm with leader
-  vim.keymap.set('n', '<leader>t', ':ToggleTerm<CR>', { desc = "Toggle Terminal", noremap = true, silent = true })
+    })
+    -- Keybinding to open toggleterm with leader
+    vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>", { desc = "Toggle Terminal", noremap = true, silent = true })
   end,
 }
